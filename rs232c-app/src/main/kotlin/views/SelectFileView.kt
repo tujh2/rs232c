33d974bb7693866a4b7eb28350b1425ebc598502
Igormamman <Iamverybyakya@gmail.com>
2021-02-28 10:42:34 +0300

    побаловался с tfx

---
+++ rs232c-app/src/main/kotlin/views/SelectFileView.kt
@@ -0,0 +1,35 @@
+package views
+
+import FileTransferApp.Companion.myApp
+import javafx.scene.Parent
+import javafx.scene.control.CheckMenuItemtat
+import javafx.scene.control.Menu
+import jssc.SerialPort
+import jssc.SerialPortList
+import tornadofx.*
+import javafx.stage.FileChooser
+
+
+
+
+
+class SelectFileView :View(){
+
+    override val root = vbox(){
+        button("Select file to transfer"){
+            action {
+                val filter = arrayOf(FileChooser.ExtensionFilter(
+                    "Portable Network Graphics (PNG)", "*.png")
+                )
+                val file=chooseFile("Select file to transfer",filter)
+                if(!file.isEmpty())
+                    println(file)
+                else println("EMPTY")
+            }
+
+
+        }
+
+    }
+
+}
