0fb87178676a565c61fe9090c08004cb041ddac1
Egor Sysoykin <egor.sysoykin@mail.ru>
2021-03-03 13:06:32 +0300

    fix: форматирование кода

--- rs232c-app/src/main/kotlin/views/SelectFileView.kt
+++ rs232c-app/src/main/kotlin/views/SelectFileView.kt
@@ -2,32 +2,26 @@ package views
 
 import FileTransferApp.Companion.myApp
 import javafx.geometry.Pos
-import javafx.scene.Parent
-import javafx.scene.control.CheckMenuItem
 import javafx.scene.control.Label
-import javafx.scene.control.Menu
-import jssc.SerialPort
-import jssc.SerialPortList
 import tornadofx.*
 import javafx.stage.FileChooser
-import java.io.File
-
 
 class SelectFileView :View(){
     private lateinit var selectedFileLabel: Label
-    override val root = vbox(alignment = Pos.TOP_CENTER){
+    override val root = vbox(alignment = Pos.TOP_CENTER) {
 
-        button("Select file to transfer"){
+        button("Select file to transfer") {
             action {
                 val filter = arrayOf(FileChooser.ExtensionFilter("Empty filter","*"))
-                val fileList=chooseFile("Select file to transfer",filter,mode=FileChooserMode.Single)
-                if (!fileList.isEmpty()) {
-                    myApp.transferedFile=fileList[0]
+                val fileList = chooseFile("Select file to transfer",filter,mode=FileChooserMode.Single)
+                if (fileList.isNotEmpty()) {
+                    myApp.transferedFile = fileList[0]
                     selectedFileLabel.text = "Selected files:${myApp.transferedFile.name}"
-                }
-                else selectedFileLabel.text = "Not selected file"
+                } else {
+                    selectedFileLabel.text = "Not selected file"
                 }
             }
+        }
         label("Not selected file") { selectedFileLabel = this }
     }
 
